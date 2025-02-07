7da3266826560b4169e4febceab336f5b0b6dd6e
Joel Lindberg <joel.lindberg@mtek.se>
2025-02-08 00:28:25 +0100

    Seperate files

--- functions.js
+++ functions.js
@@ -1,3 +1,77 @@
-function getRandomUInt() {
-  return Math.floor(Math.random() * (2 ** 31));
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const gravity = 0.1;
+const keys = [];
+let players = [];
+
+const ws = new WebSocket('ws://localhost:8081/ws');
+
+ws.onopen = () => {
+    console.log('Connected to server');
 }
+
+ws.onmessage = (message) => {
+    try {
+        const data = JSON.parse(message.data);
+        const player = createPlayerFromJson(data);
+        let found = false;
+        if (player.id === myplayer.id) {
+            return;
+        }
+        for (let j = 0; j < players.length; j++) {
+            if (players[j].id === player.id) {
+                players[j] = player;
+                return
+            }
+        }
+        players.push(player);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+let updatePlayerState = (playerData) => {
+    ws.send(JSON.stringify(playerData));
+}
+
+const myplayer = new Player(20, 20, 50, 50, 'white');
+const platform = new Platform(0, canvas.height - 100, canvas.width, 100, 'blue');
+myplayer.draw();
+platform.draw();
+
+const fps = 144;
+const interval = 1000 / fps;
+let lastTime = 0;
+
+const tick = (timestamp) => {
+    if (timestamp - lastTime >= interval) {
+        lastTime = timestamp;
+        
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        myplayer.animate(interval);
+        myplayer.move(interval);
+        platform.draw();
+
+        for (let i = 0; i < players.length; i++) {
+            players[i].draw();
+        }
+        if (myplayer.speed > 0) {
+            updatePlayerState(myplayer);
+        }
+    }
+
+    requestAnimationFrame(tick);
+};
+
+tick(0);
+
+window.addEventListener('keydown', (e) => {
+    keys[e.code] = true;
+});
+
+window.addEventListener('keyup', (e) => {
+    keys[e.code] = false;
+});
---
+++ src/scripts/player/player.js
@@ -0,0 +1,109 @@
+class Player {
+    constructor(x, y, width, height, color) {
+        this.id = Math.random();
+        this.name = "Player" + this.id;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.dy = 0;
+        this.jumpForce = 10;
+        this.maxSpeed = 25;
+        this.friction = 0.12;
+        this.speed = 0;
+        this.direction = 0;
+        this.maxAcceleration = 4;
+        this.acceleration = 0;
+        this.originalHeight = height;
+        this.grounded = false;
+        this.jumpTimer = 0;
+    }
+
+    animate(interval) { 
+        this.dy += gravity * interval;
+        this.y += this.dy;
+        if (this.grounded) this.speed *= interval * this.friction;
+
+        this.speed = this.speed < 0.001 ? 0 : this.speed;
+        this.x += Math.min(this.maxSpeed * interval, this.speed) * this.direction;
+        
+        if (this.jumpTimer > 0) {
+            this.jumpTimer--;
+            this.dy = -this.jumpForce;
+        }
+
+        if (this.y + this.height > canvas.height - 100) {
+            this.y = canvas.height - this.height - 100;
+            this.grounded = true;
+            this.dy = 0;
+        }
+
+        this.draw();
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    move(interval) {
+        if (keys['ArrowUp'] && this.grounded) {
+            this.jumpTimer = 12;
+            this.y -= 10;
+            this.grounded = false;
+        }
+        if (keys['ArrowRight']) {
+            if (this.acceleration > 0 && this.direction === -1) {
+                this.speed -= this.acceleration;
+                if (this.speed < 0) {
+                    this.speed = 0;
+                    this.acceleration = 0;
+                }
+                return;
+            }
+
+            if (this.grounded) {
+                this.direction = 1;
+                this.acceleration = Math.min(this.maxAcceleration, this.acceleration + 0.01 * interval);
+                this.speed += this.acceleration;
+                return;
+            }
+        }
+        if (keys['ArrowLeft']) {
+            if (this.acceleration > 0 && this.direction === 1) {
+                this.speed -= this.acceleration;
+                if (this.speed < 0) {
+                    this.speed = Math.abs(this.speed);
+                    this.acceleration = 0;
+                }
+                return;
+            } 
+
+            if (this.grounded) {
+                this.direction = -1;
+                this.acceleration = Math.min(this.maxAcceleration, this.acceleration + 0.01 * interval);
+                this.speed += this.acceleration;
+                return;
+            }
+        }
+        this.acceleration = 0;
+    }
+}
+
+function createPlayerFromJson(json) {
+    const player = new Player(json.x, json.y, json.width, json.height, json.color);
+    player.id = json.id;
+    player.name = json.name;
+    player.dy = json.dy;
+    player.jumpForce = json.jumpForce;
+    player.maxSpeed = json.maxSpeed;
+    player.friction = json.friction;
+    player.speed = json.speed;
+    player.direction = json.direction;
+    player.acceleration = json.acceleration;
+    player.originalHeight = json.originalHeight;
+    player.grounded = json.grounded;
+    player.jumpTimer = json.jumpTimer;
+    return player;
+}
